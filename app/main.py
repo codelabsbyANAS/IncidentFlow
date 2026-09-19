c6515293da077bc47e7c867dccbe15f8d33c6a39
codewithANAS003 <chougleanas461@gmail.com>
2026-09-19 14:12:24 +0530

    Add CORS support for frontend

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ from app.routers.notifications import router as notifications_router
 from app.routers.organizations import router as organizations_router
 from app.routers.sla import router as sla_router
 from app.routers.users import router as users_router
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="IncidentFlow API",
@@ -18,6 +18,16 @@ app = FastAPI(
     version="1.0.0"
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Register application routers
 app.include_router(auth_router)
